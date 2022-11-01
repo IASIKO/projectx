50ddf4d4ac9ff5c00aa908201c4a5fcc807ee3bd
IASIKO <iaseshviligi@gmail.com>
2022-11-01 15:07:26 +0400

    add

---
+++ src/validation.js
@@ -0,0 +1,69 @@
+const numbers = [1,2,3,4,5,6,7,8,9];
+const moreNumbers = [10, 11, 12];
+// (1)//Join these two arrays joinedArrays
+
+const users = [
+    {
+        id: 23,
+        name: 'Giase'
+    },
+    {
+        id: 2,
+        name: 'Vivi'
+    },
+    {
+        id: 39,
+        name: 'Vardo'
+    }
+];
+// (2)//დაუმატე ახალი იუზერი, რომელსაც ექნება შემდეგი უნიკალური id, და სახელი ანუკი.
+//ორივე შემთხვევა: ერთი ცვლადის სახელი დარჩეს იგივე (users), მეორე შემთხვევაში - newUsers.
+
+// (3)//დასორტე ზედა მასივი id-ს მიხედვით
+
+// (4)//კიდევ 1, ჩავთვალოთ რომ გაქვს იუზერის id და ამის მიხედვით users მასივიდან რომ წაშალო მთელი ამ id-ს ობიექტი.
+//const id = 39;
+
+const allNumbers = [1,2,3,4,5,7,10,11];
+// (5)//ამ ზედა მასივიდან წამოიღე მხოლოდ ლუწი ციფრები.
+//+ გახადე ყველა ციფრი შემდეგი ლუწი რიცხვი თუ უკვე ლუწი არ არის.
+
+
+const userInfos = [
+    {
+        id: 2, 
+        name: 'Mawona',
+        age: 18
+    },
+    {
+        id: 3,
+        name: 'Yrolia',
+        age: 70
+    }
+]
+const userAddresses = [
+    {
+        id: 2,
+        street: 'Gogebashvili 11',
+        city: 'Tbilisi'
+    },
+    {
+        id: 3,
+        street: 'Tskneti str 1',
+        city: 'Tskneti'
+    },
+    {
+        id: 4,
+        street: 'Abashidze',
+        city: 'Tbilisi'
+    }
+]
+// (6)// შეართე ყოველი შესაბამისი ობიექტი id-ს მიხედვით რომ გამოვიდეს სიტყვაზე: 
+//{id: 2, 
+// name: 'Mawona', 
+// age: 18, 
+// street: 'Gogebashvili 11', 
+// city: 'Tbilisi'}
+
+
+// 7
